fd8aab2ebfcc5c11be65d64e4926d54fb1547cd4
Adam Aguirre <aethershade@live.com>
2021-03-28 15:42:24 -0600

    Fixed an issue where the file wasn't being inputed correctly. Also fixed the check out method

--- HotelData.java
+++ HotelData.java
@@ -3,11 +3,12 @@ import java.io.FileNotFoundException;
 import java.util.HashMap;
 import java.util.InputMismatchException;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class HotelData {
 
-    private HashMap<Integer, Guest> rooms;
+    private HashMap<Integer, Guest> rooms = new HashMap<Integer, Guest>();
 
     public HotelData() {
         loadData();
@@ -16,13 +17,14 @@ public class HotelData {
     public void loadData() {
         try (Scanner scan = new Scanner(new File("rooms.txt"))) {
            // Iterator<Integer> it = scan.iterator();
-            //while (scan.hasNext()) {
-                rooms = new HashMap<Integer, Guest>();
+            do {
                 rooms.put(scan.nextInt(), null);
-                scan.nextLine(); // Flush buffer after the int input
-            //}
+                //System.out.println(rooms.toString());
+            } while (scan.hasNextLine());
         } catch (FileNotFoundException e) {
             System.out.println("File does not exist.");
+        } catch (NoSuchElementException e){
+            System.out.println(e);
         }
     }
 
@@ -56,7 +58,7 @@ public class HotelData {
 
 public boolean checkOut(int r){
     try {
-        if (!roomAvail(r)) {
+        if (!roomExist(r)) {
             throw new NullPointerException("Room does not exist");
         }
         rooms.put(r, null);
@@ -80,7 +82,7 @@ public boolean checkOut(int r){
         StringBuilder bookedRooms = new StringBuilder();
         for (Integer i: rooms.keySet()) {
             if (rooms.get(i) != null) {
-                bookedRooms.append(rooms.get(i).toString());
+                bookedRooms.append(i + "=" + rooms.get(i).toString() + " ");
             }
         }
         if (bookedRooms.isEmpty()) {
